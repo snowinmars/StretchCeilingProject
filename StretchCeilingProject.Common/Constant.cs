277985fe88a66577a65bbfaa5463138e565bd5af
snowinmars <stuff4work@yandex.ru>
2017-01-10 21:03:27 +0400

    Add IsDebug to Constant and Razor page. There's some elements in Razor, that I need for develop, but not in production.

--- StretchCeilingProject.Common/Constant.cs
+++ StretchCeilingProject.Common/Constant.cs
@@ -16,12 +16,22 @@ namespace StretchCeilingProject.Common
                 new Guid(0,0,0,0,0,0,0,0,0,0,1),
                 new Guid(0,0,0,0,0,0,0,0,0,0,2),
                 new Guid(0,0,0,0,0,0,0,0,0,0,3),
-                new Guid(0,0,0,0,0,0,0,0,0,0,4),
             };
+
+#if DEBUG
+            Constant.IsDebug = true;
+#else
+            Constant.IsDebug = false;
+#endif
         }
 
         public const int MaxImageLengthInBytes = 4 * 1024 * 1024; // 4 Mb
 
         public const string ConnectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=StretchCeilingProject;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        /// <summary>
+        /// I need this due to razor can not use #ifdef construction.
+        /// </summary>
+        public static bool IsDebug { get; }
     }
 }
